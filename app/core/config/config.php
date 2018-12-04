ede749d82f780c81a4393324cc19aa416e118e34
Marco <mondial95@gmail.com>
2018-12-04 16:55:19 +0100

    🛠 old stuff never committed

--- app/core/config/config.php
+++ app/core/config/config.php
@@ -1,19 +1,41 @@
 <?php
 
 /**
+ * Set session cookie properties
+ */
+
+// **PREVENTING SESSION HIJACKING**
+// Prevents javascript XSS attacks aimed to steal the session ID
+ini_set('session.cookie_httponly', 1);
+
+// **PREVENTING SESSION FIXATION**
+// Session ID cannot be passed through URLs
+ini_set('session.use_only_cookies', 1);
+
+// Uses a secure connection (HTTPS) if possible
+ini_set('session.cookie_secure', 1);
+
+/**
+ * Define headers for security issues
+ * NOTE already defined in the htaccess
+ */
+// header("X-Frame-Options: SAMEORIGIN");
+// header("X-XSS-Protection: 1");
+
+/**
  * Constants available in any application script
  */
 
 define('APP_DIR', dirname( dirname( __DIR__ ) ));
 
-define('CONTROLLERS_DIR', APP_DIR . "/controllers");
+define('CONTROLLERS_DIR', APP_DIR . '/controllers');
 
-define('VIEWS_DIR', APP_DIR . "/templates");
+define('MODELS_DIR', APP_DIR . '/models');
 
-define('MODELS_DIR', APP_DIR . "/models");
+define('VIEWS_DIR', APP_DIR . '/templates');
 
-define('CORE_DIR', APP_DIR . "/core");
+define('CORE_DIR', APP_DIR . '/core');
 
-define('DOCS_DIR', APP_DIR . "/docs");
+define('API_DIR', APP_DIR . '/api');
 
-define('API_DIR', APP_DIR . "/api");
+define('UTILS_DIR', APP_DIR . '/utils');
